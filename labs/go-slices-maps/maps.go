c91925241b8f13a656a86160a97ad169cbac9e56
Juan José Cazarez <juanjose.cazarezg@gmail.com>
2020-09-21 04:32:11 +0000

    Solve go-slices-maps
    Signed-off-by: Juan José Cazarez <juanjose.cazarezg@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	split := strings.Fields(s)
+	m := make(map[string]int)
+	for _, v := range split {
+		m[v] += 1
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,18 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	p := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		p[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			p[i][j] = uint8((82*i + j)/3)
+		}
+	}
+	return p
+}
+
 func main() {
+	pic.Show(Pic)
 }
