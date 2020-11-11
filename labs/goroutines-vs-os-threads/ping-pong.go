4456aa67efdd7c8c0bef18df527d4b3ba56c98f1
Juan José Cazarez <juanjose.cazarezg@gmail.com>
2020-11-11 04:36:31 +0000

    Solve goroutines-vs-os-threads lab
    Signed-off-by: Juan José Cazarez <juanjose.cazarezg@gmail.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func send(x chan int, y chan int) {
+	for {
+		y <- (1 + <-x)
+	}
+}
+
 func main() {
-	var commsPerSecond int
+	x := make(chan int)
+	y := make(chan int)
+	commsPerSecond := 0
+
+	go send(x, y)
+	go send(y, x)
+
+	for i := 0; i < 1; i++ {
+		x <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		messages := <-x
+		commsPerSecond += messages
+	}
 
 	fmt.Println("Communications Per Second : ", commsPerSecond)
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
 func main() {
-	var maxStages, transitTime int
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	var memory runtime.MemStats
+	runtime.ReadMemStats(&memory)
+	channel := make(chan int)
+	maxStages := 0
+
+	defer func() {
+		if goroutine := recover(); goroutine != nil {
+			fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+			startingTime := time.Now()
+			for ; maxStages > 0; maxStages-- {
+				channel <- maxStages
+			}
+			transitTime := time.Since(startingTime)
+			fmt.Println("Time to transit trough the pipeline : ", transitTime)
+		}
+
+	}()
+
+	for {
+		go func() {
+			maxStages++
+			<-channel
+		}()
+		runtime.ReadMemStats(&memory)
+		if memory.TotalAlloc/1024/1024 > 256 {
+			panic("MAXIMUM AMOUNT OF MEMORY REACHED")
+		}
+	}
 }
