6520117f667341c1ba262769aace84bef95c957d
Juan José Cazarez <juanjose.cazarezg@gmail.com>
2020-11-17 23:47:42 +0000

    Solve matrix-multiplication lab
    Signed-off-by: Juan José Cazarez <juanjose.cazarezg@gmail.com>

--- labs/matrix-multiplication/logger.c
+++ labs/matrix-multiplication/logger.c
@@ -1,23 +1,133 @@
 #include <stdio.h>
 #include "logger.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <syslog.h>
+
+
+#define RED 1
+#define YELLOW 3
+#define MAGENTA 5
+#define WHITE 7
+#define GREEN 2
+
+int systemlog = 0;
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+
+    if (strcmp(logType, "syslog") == 0)
+    {
+        systemlog = 1;
+        printf("Initializing Logger on: syslog\n");
+    }
+    else if (strcmp(logType, "stdout") == 0 || strcmp(logType, "") == 0)
+    {
+        printf("Initializing Logger on: stdout\n");
+    }
+    else
+    {
+        printf("Not a proper value");
+        return 1;
+    }
+
     return 0;
 }
 
+void txtcolor(int fg)
+{
+    char color[13];
+    sprintf(color, "%c[%dm", 0x1B, fg + 30);
+    printf("%s", color);
+}
+
 int infof(const char *format, ...) {
+    txtcolor(GREEN);
+    va_list arg;
+    va_start(arg, format);
+
+    if (systemlog == 1)
+    {
+        openlog("mylogger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    else
+    {
+        vprintf(format, arg);
+        printf("\n");
+    }
+
+    va_end(arg);
+    txtcolor(WHITE);
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    txtcolor(YELLOW);
+    va_list arg;
+    va_start(arg, format);
+
+    if (systemlog == 1)
+    {
+        openlog("mylogger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_WARNING, format, arg);
+        closelog();
+    }
+    else
+    {
+        vprintf(format, arg);
+        printf("\n");
+    }
+
+    va_end(arg);
+    txtcolor(WHITE);
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    txtcolor(RED);
+    va_list arg;
+    va_start(arg, format);
+
+    if (systemlog == 1)
+    {
+        openlog("mylogger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_ERR, format, arg);
+        closelog();
+    }
+    else
+    {
+        vprintf(format, arg);
+        printf("\n");
+    }
+
+    va_end(arg);
+    txtcolor(WHITE);
     return 0;
 }
 int panicf(const char *format, ...) {
+    txtcolor(MAGENTA);
+    va_list arg;
+    va_start(arg, format);
+
+    if (systemlog == 1)
+    {
+        openlog("mylogger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG, format, arg);
+        closelog();
+    }
+    else
+    {
+        vprintf(format, arg);
+        printf("\n");
+    }
+
+    va_end(arg);
+    txtcolor(WHITE);
+    raise(SIGABRT);
     return 0;
 }
 
--- labs/matrix-multiplication/multiplier.c
+++ labs/matrix-multiplication/multiplier.c
@@ -1,7 +1,192 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "logger.h"
+#include <pthread.h>
 
-int main(){
-    // Place your magic here
+long * readMatrix(char *filename);
+long * getColumn(int col, long *matrix);
+long * getRow(int row, long *matrix);
+int getLock();
+int releaseLock(int lock);
+long dotProduct(long *vec1, long *vec2);
+long * multiply(long *matA, long *matB);
+int saveResultMatrix(long *result);
+
+int NUM_BUFFERS;
+char* RESULT_MATRIX_FILE;
+int MATRIX_DIMENSION=2000;
+
+long **buffers;
+pthread_mutex_t *mutex;
+pthread_t thread[2000];
+
+typedef struct {
+    int row;
+    int col;
+    long *matrixA;
+    long *matrixB;
+
+}args;
+
+int main(int argc, char **argv){
+    initLogger("stdout");
+    if(argc<5){
+        errorf("Insert arguments like './multiplier -n 10 -out result.txt'");
+        exit(1);
+    }
+    else{
+        for(int i=1; i<argc;i++){
+            if(strcmp(argv[i],"-n")==0){
+                i++;
+                if(i>=argc){
+                    errorf("Insert arguments like './multiplier -n 10 -out result.txt'");
+                    exit(1);
+                }
+                NUM_BUFFERS=atoi(argv[i]);
+            }
+            else if(strcmp(argv[i],"-out")==0){
+                i++;
+                if(i>=argc){
+                    errorf("Insert arguments like './multiplier -n 10 -out result.txt'");
+                    exit(1);
+                }
+                RESULT_MATRIX_FILE=argv[i];
+            }
+        }
+    }
+
+    buffers=malloc(NUM_BUFFERS*sizeof(long *));
+    mutex=malloc(NUM_BUFFERS*sizeof(pthread_mutex_t));
+    long *matrixA=readMatrix("matA.dat");
+    long *matrixB=readMatrix("matB.dat");
+    for(int i = 0; i < NUM_BUFFERS; i++){
+        pthread_mutex_init(&mutex[i], NULL);
+    }
+
+    
+    long *result = multiply(matrixA,matrixB);
+    infof("Finished multiplication\n");
+    saveResultMatrix(result);
+    free(matrixA);
+    free(matrixB);
+    free(mutex);
+    free(buffers);
+    return 0;
+    
+}
+long multiplicationFragment(args *arg){
+    int buffer1 = -1,
+        buffer2 = -1;
+    while(buffer1 == -1 || buffer2 ==-1){
+        if(buffer1 == -1){
+            buffer1 = getLock();
+        }
+        if(buffer2 == -1){
+            buffer2 = getLock();
+        }
+    }
+    buffers[buffer1] = getRow(arg->row,arg->matrixA);
+    buffers[buffer2] = getColumn(arg->col,arg->matrixB);
+    long result = dotProduct(buffers[buffer1],buffers[buffer2]);
+    free(buffers[buffer1]);
+    free(buffers[buffer2]);
+    free(arg);
+    releaseLock(buffer1);
+    releaseLock(buffer2);
+    return result;
+}
+
+long * multiply(long *matA, long *matB){
+    infof("Starting multiplication. This may take several minutes\n");
+    long *result= malloc(MATRIX_DIMENSION*MATRIX_DIMENSION*sizeof(long));
+    for(int i=0; i<MATRIX_DIMENSION; i++){
+        for(int j=0;j<MATRIX_DIMENSION;j++){
+            //pthread_create(&thread[j],NULL,)
+            args *ar=malloc(sizeof(args));
+            ar -> row = i;
+            ar -> col = j;
+            ar -> matrixA = matA;
+            ar -> matrixB = matB;
+            pthread_create(&thread[j],NULL,(void * (*) (void *))multiplicationFragment,(void *)ar);
+        }
+        for(int j=0; j<MATRIX_DIMENSION; j++){
+            void *res;
+            pthread_join(thread[j], &res);
+            result[MATRIX_DIMENSION * j + i] = (long) res;
+        }
+    }
+    return result;
+}
+long * readMatrix( char *filename){
+    int size=0;
+    FILE *f =fopen(filename,"r");
+    if(f==NULL){
+        errorf("Error opening the Matrix data file");
+        exit(2);
+    }
+    char c;
+    while((c=fgetc(f))!=EOF){
+        if(c=='\n')
+            size++;
+    }
+    rewind(f);
+    long *matrix=malloc(size*sizeof(long));
+    int index=0;
+    while(fscanf(f,"%ld",&matrix[index])!= EOF){
+        index++;
+    }
+    fclose(f);
+    return matrix;   
+}
+
+long * getRow(int row, long *matrix){
+    long *r=malloc(MATRIX_DIMENSION*sizeof(long));
+    int rowStart=MATRIX_DIMENSION*(row);
+    for(int i=0;i<MATRIX_DIMENSION;i++){
+        r[i]=matrix[rowStart+i];
+    }
+    return r;
+}
+
+long * getColumn(int column, long *matrix){
+    long *c=malloc(MATRIX_DIMENSION*sizeof(long));
+    for(int i=0;i<MATRIX_DIMENSION;i++){
+        c[i]=matrix[column];
+        column+=MATRIX_DIMENSION;
+    }
+    return c;
+}
+
+int getLock() {
+    for(int i = 0; i < NUM_BUFFERS; i++){
+        if(pthread_mutex_trylock(&mutex[i])==0){
+            return i;
+        }   
+    }
+    return -1;
+}
+int releaseLock(int lock) {
+    return pthread_mutex_unlock(&mutex[lock]);
+}
+long dotProduct(long *vec1, long *vec2){
+    long result = 0;
+    for(int i=0; i<MATRIX_DIMENSION; i++){
+        result+=vec1[i]*vec2[i];
+    }
+    return result;
+}
+
+int saveResultMatrix(long *result) {    
+    FILE *file;
+    file = fopen(RESULT_MATRIX_FILE, "w+");
+    if(file==NULL){
+        return -1;
+    }
+    int size=MATRIX_DIMENSION*MATRIX_DIMENSION;
+    for(int i = 0; i < size; i++) {
+        fprintf(file, "%ld\n", result[i]);
+    }
+    fclose(file);
     return 0;
 }
